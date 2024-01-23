2af23ec15c931c46e5517ce8c8fcf70d118dbec0
RahulGupta <rahulrgd21@gmail.com>
2024-01-24 04:43:43 +0530

    Step 19 - Setting up JWT Resource with Spring Security and Spring Boot - 1

--- src/main/java/com/in28minutes/learningspringsecurity/jwt/JwtSecurityConfiguration.java
+++ src/main/java/com/in28minutes/learningspringsecurity/jwt/JwtSecurityConfiguration.java
@@ -26,7 +26,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -112,5 +114,10 @@ public class JwtSecurityConfiguration {
   @Bean
   public JwtDecoder jwtDecoder(RSAKey rsakey) throws JOSEException {
     return NimbusJwtDecoder.withPublicKey(rsakey.toRSAPublicKey()).build();
+  }
+
+  @Bean
+  public JwtEncoder jwtEncoder(JWKSource<SecurityContext>jwkSource){
+    return new NimbusJwtEncoder(jwkSource);
   }
 }
