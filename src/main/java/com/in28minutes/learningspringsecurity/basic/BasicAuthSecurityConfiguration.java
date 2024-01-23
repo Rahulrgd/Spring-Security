9b606d7c16e68a0dc214b833ccc83f7ed3a8f531
RahulGupta <rahulrgd21@gmail.com>
2024-01-24 01:35:36 +0530

    Step 13 - Exploring Spring Security - Storing User Credentials using JDBC

--- src/main/java/com/in28minutes/learningspringsecurity/basic/BasicAuthSecurityConfiguration.java
+++ src/main/java/com/in28minutes/learningspringsecurity/basic/BasicAuthSecurityConfiguration.java
@@ -1,15 +1,20 @@
 package com.in28minutes.learningspringsecurity.basic;
 
+import javax.sql.DataSource;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -27,13 +32,51 @@ public class BasicAuthSecurityConfiguration {
     // http.formLogin();
     http.httpBasic();
     http.csrf().disable();
+    http.headers().frameOptions().sameOrigin();
     return http.build();
   }
 
+  //   Creating new user in inmemory
+//   @Bean
+//   public UserDetailsService userDetailsService() {
+//     var user = User
+//       .withUsername("in28minutes")
+//       .password("{noop}dummy")
+//       .roles("USER")
+//       .build();
+//     var admin = User
+//       .withUsername("admin")
+//       .password("{noop}dummy")
+//       .roles("ADMIN")
+//       .build();
+//     return new InMemoryUserDetailsManager(user, admin);
+//   }
+
+  //   Executing Default User Schema DDL to create User table in H2 database during thing startup of the application
   @Bean
-  public UserDetailsService userDetailsService() {
-    var user = User.withUsername("in28minutes").password("{noop}dummy").roles("USER").build();
-    var admin = User.withUsername("admin").password("{noop}dummy").roles("ADMIN").build();
-    return new InMemoryUserDetailsManager(user, admin);
+  public DataSource dataSource() {
+    return new EmbeddedDatabaseBuilder()
+      .setType(EmbeddedDatabaseType.H2)
+      .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+      .build();
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService(DataSource dataSource) {
+    var user = User
+      .withUsername("in28minutes")
+      .password("{noop}dummy")
+      .roles("USER","ADMIN")
+      .build();
+    var admin = User
+      .withUsername("admin")
+      .password("{noop}dummy")
+      .roles("ADMIN")
+      .build();
+
+      var jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+      jdbcUserDetailsManager.createUser(user);
+      jdbcUserDetailsManager.createUser(admin);
+    return jdbcUserDetailsManager;
   }
 }
